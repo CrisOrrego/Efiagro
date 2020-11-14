4415ef9246282b1ef43e2bd408972eeb1e708980
Christian Orrego <viciousgrant@gmail.com>
2020-11-14 11:12:25 -0500

    avance articulos

--- resources/views/Seccion.blade.php
+++ resources/views/Seccion.blade.php
@@ -1,9 +1,18 @@
 <div flex layout=column>
-		
-	<div layout>
-		<a ng-repeat="S in Secciones[Estado.ruta[2]]" href="#/Home/@{{ S.seccion_slug }}/@{{ S.subseccion_slug }}">
-			@{{ S.subseccion }}
-		</a>
+	
+	<md-button class="md-icon-button abs no-margin s30 no-padding" style="top: 8px;left: 0;"
+		href="#/Home">
+		<md-icon md-font-icon="fa-chevron-left fa-lg fa-fw text-primary"></md-icon>
+	</md-button>
+
+	<div layout class="bg-primary md-short" md-theme="Transparent" ng-show="Secciones[Estado.ruta[2]].length > 1">
+
+		<md-tabs flex >
+			<md-tab ng-repeat="S in Secciones[Estado.ruta[2]]" label="@{{ S.subseccion }}"
+				href="#/Home/@{{ S.seccion_slug }}/@{{ S.subseccion_slug }}"></md-tab>
+		</md-tabs>
+
+
 	</div>
 
 	<div id="Subseccion" flex ui-view></div>
