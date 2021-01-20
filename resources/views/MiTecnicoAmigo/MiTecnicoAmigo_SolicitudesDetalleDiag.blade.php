ed69e6dd220e01c85cb5a9b7d42ce1ae4cb05977
lhosorioq <lhosorio@gmail.com>
2021-01-19 21:57:54 -0500

    Organizar visualmente el detalle del caso

--- resources/views/MiTecnicoAmigo/MiTecnicoAmigo_SolicitudesDetalleDiag.blade.php
+++ resources/views/MiTecnicoAmigo/MiTecnicoAmigo_SolicitudesDetalleDiag.blade.php
@@ -1,45 +1,32 @@
-<md-dialog class="vh95 w100p mxw850" layout=column>
-    
-	<div layout layout-align="center center" class="margin-top">
-		<div class="text-clear padding-left" flex>
-			<h1>@{{ Caso.id }} - @{{ Caso.titulo }}</h1>
-		</div>
+<md-dialog class="vh95 mxw850 overflow-x" layout=column>
 
-		
-		<md-button ng-click="Cancel()" class="md-icon-button no-margin text-red">
+	<div class="md-toolbar-tools">
+        <h2>Caso # @{{ Caso.id }}</h2>
+        <span flex="" class="flex"></span>
+        <md-button ng-click="Cancel()" class="md-icon-button no-margin text-red">
 			<md-icon md-font-icon="fa-times fa-lg fa-fw"></md-icon>
 			<md-tooltip md-direction="bottom">Cerrar modal</md-tooltip>
 		</md-button>
 	</div>
+	<div class="text-clear padding-0-20" flex>@{{ Caso.tipo }} ( Luigi OsoQui )</div>
+	<h3 class="padding-0-20">@{{ Caso.titulo }}</h3>
 
-	<div layout=column flex class="">
-		<div layout class="padding-0-5">
-			<md-input-container class="no-margin md-title" md-no-float flex>
-				<input type="text" ng-model="Caso.titulo" placeholder="Titulo" 
-					ng-change="guardarCaso()" ng-model-options="{ debounce: 3000 }">
-			</md-input-container>
-		</div>
-		<div class="text-clear padding-left" flex>Caso de Tipo: @{{ Caso.tipo }}</div>
-		<hr>
+	<div layout=column flex class="overflow-y padding-0-20">
 		<h4 class="text-center">Novedades del Caso</h4>
-	</div>
-	<hr>
-
-	<div flex layout=column >
-		<div ng-repeat="N in NovedadesCRUD.rows" class="padding-5">
-		<p>@{{ N.novedad }}</p>
-			
-			<p>&nbsp;</p>
+		<div ng-repeat="N in NovedadesCRUD.rows" class="footnote">
+			<p>
+				@{{ N.created_at | date:'longDate' }} - Nombre Usuario <br/>
+				@{{ N.novedad }}
+			</p>
 		</div>
 	</div>
 	
-	<md-button ng-click="crearNovedad('Texto')" class="md-icon-button no-margin text-green">
-		<md-icon md-font-icon="fa-align-justify fa-lg fa-fw"></md-icon>
-		<md-tooltip md-direction="bottom">Agregar texto</md-tooltip>
-	</md-button>
-	<md-button ng-click="crearNovedad('Imagen')" class="md-icon-button no-margin text-green">
-		<md-icon md-font-icon="fa-image fa-lg fa-fw"></md-icon>
-		<md-tooltip md-direction="bottom">Agregar Imagen</md-tooltip>
-	</md-button>
-
+	<div layout class=" padding-5-10" layout-align="space-between start">
+		<md-input-container class="no-margin md-title w80p" >
+			<textarea ng-model="detallecaso" placeholder="Registrar la novedad" rows="3"></textarea>
+		</md-input-container>
+		<md-button class="md-raised md-primary no-margin" ng-click="crearNovedad('Texto', detallecaso)">
+			Agregar
+		</md-button>
+	</div>
 </md-dialog>
