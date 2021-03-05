eebaa13a8cff3b95ad08348472a077731b9deaa2
MAHS <miguellangelhs@gmail.com>
2021-03-05 12:35:48 -0500

    Actualización

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -56,28 +56,28 @@ class MainController extends Controller
         };
     }
 
-    public function postUploadImagen()
-    {
-        extract(request()->all()); //Path, Quality, Alto, Ancho
+    // public function postUploadImagen()
+    // {
+    //     extract(request()->all()); //Path, Quality, Alto, Ancho
 
-        //dd(Input::file('file'));
-        $image = Image::make(request('file')->getRealPath());
+    //     //dd(Input::file('file'));
+    //     $image = Image::make(request('file')->getRealPath());
         
-        $Ruta = dirname($Path);
-        if(!File::exists($Ruta)) File::makeDirectory($Ruta, 0775, true, true);
+    //     $Ruta = dirname($Path);
+    //     if(!File::exists($Ruta)) File::makeDirectory($Ruta, 0775, true, true);
 
-        $image->resize($Ancho, $Alto, function ($constraint) {
-            $constraint->aspectRatio();
-        });
+    //     $image->resize($Ancho, $Alto, function ($constraint) {
+    //         $constraint->aspectRatio();
+    //     });
 
-        //return $image->response('jpg', 70);
+    //     //return $image->response('jpg', 70);
 
-        if(!$image->save($Path, $Quality)){
-            return response()->json(['Msg' => 'No se pudo guardar la imagen'], 512);
-        }else{
-            return response()->json(['Msg' => $Path ], 200);
-        };
-    }
+    //     if(!$image->save($Path, $Quality)){
+    //         return response()->json(['Msg' => 'No se pudo guardar la imagen'], 512);
+    //     }else{
+    //         return response()->json(['Msg' => $Path ], 200);
+    //     };
+    // }
 
     //INICIO DEV ANGÉLICA --> Funcion para subir imagen al servidor
     public function postUploadImagen()
