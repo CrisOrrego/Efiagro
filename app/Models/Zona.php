75f77f9a1b066b43acb0aab16f7adc17c453bc1a
MAHS <miguellangelhs@gmail.com>
2021-03-23 21:13:21 -0500

    app min

--- app/Models/Zona.php
+++ app/Models/Zona.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Zona extends Model
 {
     use HasFactory;
-    protected $table = 'zonas_agroambientales';
+    protected $table = 'zonas';
     protected $guarded = ['id'];
     protected $appends = [];
 
