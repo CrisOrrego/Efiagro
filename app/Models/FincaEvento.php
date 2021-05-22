c6d3b5450823ed5abe6b3cdec7c5965020e0989d
MAHS <miguellangelhs@gmail.com>
2021-05-21 20:33:06 -0500

    FincaEventos

--- app/Models/FincaEvento.php
+++ app/Models/FincaEvento.php
@@ -21,45 +21,17 @@ class Lote extends Model
 
         //Name, Desc, Type, Required, Unique, Default, Width, Options
         return [
-            ['finca_id',            'Finca',            'select',   true,   false,  null, 50, ['options' => $fincas] ],
-            ['organizacion_id',     'Organización',     'select',   true,   false,  null, 50, ['options' => $organizaciones] ],
-            ['linea_productiva_id', 'Linea Productiva', 'select',   true,   false,  null, 50, ['options' => $lineasproductivas] ],
-            ['labores_id',          'Labores',         'select',    true,   false,  null, 50, ['options' => $labores] ], 
-            ['hectareas',           'Hectareas',        null,       true,     false,  null,       100],
-            ['sitios',              'Sitios',           null,       true,     false,  null,       100],
-            ['coordenadas',         'Coordenadas',      null,       true,     false,  null,       100],
+           
+            ['finca_id',           'Finca',        null,       true,     false,  null,       100],
+            ['evento_id',              'evento_id',           null,       true,     false,  null,       100],
+            ['fecha',         'fecha',      null,       true,     false,  null,       100],
+            ['gravedad',         'gravedad',      null,       true,     false,  null,       100],
+            
             
         ];
 
     }
 
-    public function scopeFinca_id($q, $id)
-    {
-        return $q->where('finca_id', $id);
-    }
-
-    public function finca()
-    {
-        return $this->belongsTo('App\Models\Finca', 'finca_id');
-    }
-    public function organizacion()
-    {
-        return $this->belongsTo('App\Models\Organizacion', 'organizacion_id');
-    }
-
-    public function linea_productiva()
-    {
-        return $this->belongsTo('App\Models\LineaProductiva', 'linea_productiva_id');
-    }
-    
-    public function labor()
-    {
-        return $this->belongsTo('App\Models\Labor', 'labores_id');
-    }
-
-    public function tareas()
-    {
-        return $this->hasMany('App\Models\TareasLote', 'lote_id');
-    }
+   
 }
 
